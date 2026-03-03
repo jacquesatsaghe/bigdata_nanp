87b93d659f00280431899173483226e72184a2db
Patrick <ngounepatrick@gmail.com>
2026-03-03 20:30:48 +0000

    updating iceberg nessie & polaris

--- iceberg/sql/postgrescat/postgresql-init.sql
+++
@@ -1,9 +0,0 @@
--- 1. Apache Polaris Catalog for Apache Iceberg Datastore
-
--- psql -h localhost -p 5432 -U dbadmin -d findept
-
-CREATE DATABASE findept;
-GRANT ALL PRIVILEGES ON DATABASE findept TO dbadmin;
-
--- The Polaris stack creates the schema as part of it's bootstrap/setup process, 
--- will be called polaris_schema
